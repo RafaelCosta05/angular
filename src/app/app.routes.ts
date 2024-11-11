3df3027aa379aa12a526d0cf9ec609382b62746c
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-11-11 21:59:47 +0000

    Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,11 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
 import { MainComponent } from './components/dashboard/pages/main/main.component';
 import { ReservationsComponent } from './components/dashboard/pages/reservations/reservations.component';
+import { CourtsComponent } from './components/dashboard/pages/courts/courts.component';
+import { PromotionsComponent } from './components/dashboard/pages/promotions/promotions.component';
+import { StatisticsComponent } from './components/dashboard/pages/statistics/statistics.component';
+import { CustomersComponent } from './components/dashboard/pages/customers/customers.component';
+import { SettingsComponent } from './components/dashboard/pages/settings/settings.component';
 
 export const routes: Routes = [
   {
@@ -34,7 +39,12 @@ export const routes: Routes = [
     title: 'Dashboard',
     children: [
       { path: 'main', component: MainComponent },
-      { path: 'reservations', component: ReservationsComponent, title: 'Reservations' },
+      { path: 'reservations', component: ReservationsComponent, title: 'Dashboard - Reservas' },
+      { path: 'courts', component: CourtsComponent, title: 'Dashboard - Campos' },
+      { path: 'promotions', component: PromotionsComponent, title: 'Dashboard - Promoções' },
+      { path: 'statistics', component: StatisticsComponent, title: 'Dashboard - Estatísticas' },
+      { path: 'customers', component: CustomersComponent, title: 'Dashboard - Clientes' },
+      { path: 'settings', component: SettingsComponent, title: 'Dashboard - Definições' },
 
       { path: '', redirectTo: 'main', pathMatch: 'full' },
     ]
