16a7b8e433f88cffd1a136070db52f32ba71b975
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-11-12 12:17:45 +0000

    Update

--- src/app/components/dashboard/sidebar/sidebar.component.ts
+++ src/app/components/dashboard/sidebar/sidebar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  constructor(private router: Router) {}
 
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
