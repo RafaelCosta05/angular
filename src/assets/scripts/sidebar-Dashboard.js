430bf0dd6a9ffe214280ff593a3dbd90338d14ab
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-11-12 18:06:57 +0000

    Update

--- src/assets/scripts/sidebar-Dashboard.js
+++ src/assets/scripts/sidebar-Dashboard.js
@@ -5,6 +5,7 @@ if (window.location.pathname.includes('/dashboard')) {
     const mainContainer = document.querySelector('.main-container');
     const nameSite = document.querySelector('.site-name');
     const imgLogo = document.querySelector('.logo-img');
+    const ps = document.querySelectorAll('.p-sidebar');
 
     sidebar.classList.toggle('closed');
 
@@ -14,12 +15,18 @@ if (window.location.pathname.includes('/dashboard')) {
         nameSite.style.display = 'none';
         imgLogo.style.width = '65px';
         imgLogo.style.height = '65px';
+        ps.forEach(p => {
+          p.classList.add('minimized');
+        });
     } else {
         mainContainer.style.marginLeft = '270px';
         nameSite.style.display = 'flex';
         imgLogo.style.width = '80px';
         imgLogo.style.height = '80px';
         mainContainer.classList.remove('maximized');
+        ps.forEach(p => {
+          p.classList.remove('minimized');
+        });
     }
   }
 
