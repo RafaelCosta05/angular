b90a9ac5f697f1dc4306ddd7233b2eade38fc024
Rafael Costa <Rafael.Costa.T0127545@edu.atec.pt>
2024-11-12 14:03:24 +0000

    Update

--- src/app/components/dashboard/header/header.component.ts
+++ src/app/components/dashboard/header/header.component.ts
@@ -1,25 +1,13 @@
 import { Component } from '@angular/core';
-import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
-  providers: [DatePipe]  // Fornecendo o DatePipe diretamente no componente
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  date: string;
-  hour: string;
-
-  constructor(private datePipe: DatePipe) {
-    const currentDate = new Date();
-
-    // Formatar a data para o formato "23 de Abril"
-    this.date = this.datePipe.transform(currentDate, 'd \'de\' MMMM', 'pt-BR')!;
-
-    // Formatar a hora para o formato "21:13"
-    this.hour = this.datePipe.transform(currentDate, 'HH:mm')!;
-  }
+  date: string = "12 de Novembro";
+  hour: string = "16:00";
 }
